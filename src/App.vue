8fc1cb04743bbd266cdfc4d4e7db7d8a859bd477
Connor Shea <connor.james.shea@gmail.com>
2021-09-05 16:32:00 -0600

    Fix `yarn run build`.
    Defines custom components property so TypeScript stops complaining.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts">
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+<script lang="ts">
+  import { defineComponent } from 'vue';
   import TeaShop from './components/TeaShop.vue';
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      TeaShop
+    }
+  });
 </script>
 
 <template>
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,18 @@
 /// <reference types="vite/client" />
 
+import { ComponentCustomProperties } from 'vue';
+
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $filters: {
+      pluralize(amount: number, singular: string, plural: string | null = null): string
+    }
+  }
 }
